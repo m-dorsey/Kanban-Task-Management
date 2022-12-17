2c454e1b186adbe5052fa133123c3ff16f304f2a
m-dorsey <dorseymakenzie@gmail.com>
2022-12-16 21:03:36 -0800

    implemented tutorial kanban code and styling

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,10 +1,35 @@
-import { Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Board } from '../models/board.model';
+import { Column } from '../models/column.model';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+      
+  }
+
+  board: Board = new Board('Test Board', [
+    new Column('Ideas', [
+      "Lionel",
+      "Messi"
+    ]),
+    new Column('To Do', [
+      "Julian",
+      "Alvarez"
+    ]),
+    new Column('Done', [
+      "Angel",
+      "Di Maria"
+    ])
+  ]);
 
 }
